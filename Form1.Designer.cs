38f3f3ebd3d532358957d651bbbdc59f85aeb9d3
bmac313 <mcb0193@comcast.net>
2016-06-22 15:40:12 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace ToDoListCS
+{
+    partial class frmMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstList = new System.Windows.Forms.ListBox();
+            this.txtTextInput = new System.Windows.Forms.TextBox();
+            this.lblTextInput = new System.Windows.Forms.Label();
+            this.mnuMainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.lstTaskNumbers = new System.Windows.Forms.ListBox();
+            this.lblTaskNumber = new System.Windows.Forms.Label();
+            this.lblTaskList = new System.Windows.Forms.Label();
+            this.mnuMainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstList
+            // 
+            this.lstList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lstList.FormattingEnabled = true;
+            this.lstList.ItemHeight = 20;
+            this.lstList.Location = new System.Drawing.Point(348, 78);
+            this.lstList.Name = "lstList";
+            this.lstList.Size = new System.Drawing.Size(489, 464);
+            this.lstList.TabIndex = 0;
+            // 
+            // txtTextInput
+            // 
+            this.txtTextInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtTextInput.Location = new System.Drawing.Point(47, 68);
+            this.txtTextInput.Name = "txtTextInput";
+            this.txtTextInput.Size = new System.Drawing.Size(216, 26);
+            this.txtTextInput.TabIndex = 1;
+            // 
+            // lblTextInput
+            // 
+            this.lblTextInput.AutoSize = true;
+            this.lblTextInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblTextInput.Location = new System.Drawing.Point(43, 38);
+            this.lblTextInput.Name = "lblTextInput";
+            this.lblTextInput.Size = new System.Drawing.Size(145, 20);
+            this.lblTextInput.TabIndex = 2;
+            this.lblTextInput.Text = "Type a task here...";
+            // 
+            // mnuMainMenu
+            // 
+            this.mnuMainMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mnuMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mnuMainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMainMenu.Name = "mnuMainMenu";
+            this.mnuMainMenu.Size = new System.Drawing.Size(849, 28);
+            this.mnuMainMenu.TabIndex = 3;
+            this.mnuMainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuNew,
+            this.mnuOpen,
+            this.mnuSave,
+            this.mnuExit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // mnuNew
+            // 
+            this.mnuNew.Name = "mnuNew";
+            this.mnuNew.Size = new System.Drawing.Size(129, 26);
+            this.mnuNew.Text = "New";
+            this.mnuNew.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(129, 26);
+            this.mnuOpen.Text = "Open...";
+            this.mnuOpen.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.Size = new System.Drawing.Size(129, 26);
+            this.mnuSave.Text = "Save...";
+            this.mnuSave.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(129, 26);
+            this.mnuExit.Text = "Exit";
+            this.mnuExit.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(47, 142);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(216, 93);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add task to list ->";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(47, 241);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(216, 93);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.Text = "<- Remove selected task";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(47, 354);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(216, 50);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update selected task...";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // lstTaskNumbers
+            // 
+            this.lstTaskNumbers.Enabled = false;
+            this.lstTaskNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lstTaskNumbers.FormattingEnabled = true;
+            this.lstTaskNumbers.ItemHeight = 20;
+            this.lstTaskNumbers.Location = new System.Drawing.Point(295, 78);
+            this.lstTaskNumbers.Name = "lstTaskNumbers";
+            this.lstTaskNumbers.Size = new System.Drawing.Size(44, 464);
+            this.lstTaskNumbers.TabIndex = 8;
+            // 
+            // lblTaskNumber
+            // 
+            this.lblTaskNumber.AutoSize = true;
+            this.lblTaskNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblTaskNumber.Location = new System.Drawing.Point(308, 38);
+            this.lblTaskNumber.Name = "lblTaskNumber";
+            this.lblTaskNumber.Size = new System.Drawing.Size(18, 20);
+            this.lblTaskNumber.TabIndex = 10;
+            this.lblTaskNumber.Text = "#";
+            // 
+            // lblTaskList
+            // 
+            this.lblTaskList.AutoSize = true;
+            this.lblTaskList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblTaskList.Location = new System.Drawing.Point(510, 38);
+            this.lblTaskList.Name = "lblTaskList";
+            this.lblTaskList.Size = new System.Drawing.Size(45, 20);
+            this.lblTaskList.TabIndex = 11;
+            this.lblTaskList.Text = "Task";
+            // 
+            // frmMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 562);
+            this.Controls.Add(this.lblTaskList);
+            this.Controls.Add(this.lblTaskNumber);
+            this.Controls.Add(this.lstTaskNumbers);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblTextInput);
+            this.Controls.Add(this.txtTextInput);
+            this.Controls.Add(this.lstList);
+            this.Controls.Add(this.mnuMainMenu);
+            this.MainMenuStrip = this.mnuMainMenu;
+            this.Name = "frmMainForm";
+            this.Text = "Visual To-Do List";
+            this.Load += new System.EventHandler(this.frmMainForm_Load);
+            this.mnuMainMenu.ResumeLayout(false);
+            this.mnuMainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstList;
+        private System.Windows.Forms.TextBox txtTextInput;
+        private System.Windows.Forms.Label lblTextInput;
+        private System.Windows.Forms.MenuStrip mnuMainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.ToolStripMenuItem mnuNew;
+        private System.Windows.Forms.ListBox lstTaskNumbers;
+        private System.Windows.Forms.Label lblTaskNumber;
+        private System.Windows.Forms.Label lblTaskList;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,282 @@
+﻿//-----VISUAL TO-DO LIST, v0.1-----
+//--------Brendan M., 2016---------
+
+//> Possible feature: save list with numbering, separate numbers from tasks when loading.
+//> Possible feature: export list as html, rtf, etc.
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoListCS
+{
+    public partial class frmMainForm : Form
+    {
+        public frmMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            String task = txtTextInput.Text;
+            if (String.IsNullOrWhiteSpace(task))
+            {
+                MessageBox.Show("The input box is empty. Please type something first!", "Error");
+            }
+            else
+            {
+                lstList.Items.Add(task);
+                lstTaskNumbers.Items.Add(lstList.Items.Count);
+                txtTextInput.Clear();
+                txtTextInput.Focus();
+                checkListForItems(lstList, btnAdd, btnRemove, btnUpdate);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ((lstList.SelectedIndex) != -1)
+            {
+                char choice = promptYesOrNo("Are you sure you want to remove that task? This cannot be undone.");
+                if (choice == 'y')
+                {
+                    int selection = lstList.SelectedIndex;
+                    lstList.Items.RemoveAt(selection);
+                    lstTaskNumbers.Items.RemoveAt(lstList.Items.Count);
+                    checkListForItems(lstList, btnAdd, btnRemove, btnUpdate);
+                }
+                else if (choice == 'n')
+                {
+                    MessageBox.Show("Cancelled.", "Action cancelled");
+                }
+            }
+            else
+            {
+                MessageBox.Show("You haven't selected an item to remove yet!", "Error");
+            }
+        }
+
+        public void toggleButton(Button btn, bool isOn) //true enables button; false disables it.
+        {
+            btn.Enabled = isOn;
+        }
+
+        //check the number of items in the list and enable / disable buttons depending on how many.
+        //this method takes in the list and three buttons; these buttons represent the three buttons on 
+        //the GUI.
+        public void checkListForItems(ListBox lst, Button btn1, Button btn2, Button btn3)
+        {
+            if (lst.Items.Count <= 0)
+            {
+                toggleButton(btn1, true);
+                toggleButton(btn2, false);
+                toggleButton(btn3, false);
+            }
+            if ((lst.Items.Count > 0) && (lst.Items.Count < 23))
+            {
+                toggleButton(btn1, true);
+                toggleButton(btn2, true);
+                toggleButton(btn3, true);
+            }
+            if (lst.Items.Count >= 23)
+            {
+                toggleButton(btn1, false);
+                toggleButton(btn2, true);
+                toggleButton(btn3, true);
+            }
+        }
+
+        private void frmMainForm_Load(object sender, EventArgs e)
+        {
+            checkListForItems(lstList, btnAdd, btnRemove, btnUpdate);
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveList();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if ((lstList.Items.Count) != 0)
+            {
+                char choice = promptYesOrNo("Would you like to save your current list before opening a new one?");
+                
+                if (choice == 'y')
+                {
+                    saveList();
+                    openList();
+                }
+                else if (choice == 'n')
+                {
+                    openList();
+                }
+            }
+            else
+            {
+                openList();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if ((lstList.Items.Count) > 0)
+            {
+                char choice = promptYesOrNo("Would you like to save your list before exiting?");
+                if (choice == 'y')
+                {
+                    saveList();
+                    this.Close();
+                }
+                else if (choice == 'n')
+                {
+                    this.Close();
+                }
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if ((lstList.Items.Count) != 0)
+            {
+                char choice = promptYesOrNo("Would you like to save your current list before creating a new one?");
+                
+                if (choice == 'y')
+                {
+                    saveList();
+                    newList();
+                }
+                else if (choice == 'n')
+                {
+                    newList();
+                }
+            }
+            else
+            {
+                newList();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if((lstList.SelectedIndex) != -1)
+            {
+                int selection = lstList.SelectedIndex;
+                string response = Microsoft.VisualBasic.Interaction.InputBox("Please enter an updated task.", "Update Task");
+                //only reassign task if the user types something.
+                //this helps avoid accidentally setting the task to white space if the user cancels.
+                if (!(String.IsNullOrWhiteSpace(response)))
+                {
+                    lstList.Items[selection] = response;
+                }
+            }
+            else
+            {
+                MessageBox.Show("You haven't selected an item to update yet!", "Error");
+            }
+        }
+
+        private void btnStatusSet_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This feature will be added at a later time.", "Feature coming soon!");
+        }
+
+        //returns a char, 'y', 'n', or 'c', depending on the user's choice.
+        public char promptYesOrNo(string message)
+        {
+            char choice = 'c'; //default value is 'c' (cancel) for safety purposes.
+
+            DialogResult dResult = MessageBox.Show(message, "Confirm Action", MessageBoxButtons.YesNoCancel);
+            if(dResult == DialogResult.Yes)
+            {
+                choice = 'y';
+            }
+            else if(dResult == DialogResult.No)
+            {
+                choice = 'n';
+            }
+            else if(dResult == DialogResult.Cancel)
+            {
+                choice = 'c';
+            }
+            else
+            {
+                choice = 'c';
+            }
+
+            return choice;
+        }
+
+        public void saveList()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text files (*.txt)|*.txt";
+            sfd.RestoreDirectory = true;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter saveList = new StreamWriter(sfd.OpenFile());
+                for (int i = 0; i < lstList.Items.Count; i++)
+                {
+                    saveList.WriteLine(lstList.Items[i]);
+                }
+                saveList.Dispose();
+                saveList.Close();
+            }
+
+            checkListForItems(lstList, btnAdd, btnRemove, btnUpdate);
+        }
+
+        public void newList()
+        {
+            lstList.Items.Clear();
+            lstTaskNumbers.Items.Clear();
+            checkListForItems(lstList, btnAdd, btnRemove, btnUpdate);
+        }
+
+        public void openList()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text files (*.txt)|*.txt";
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                lstList.Items.Clear();
+                lstTaskNumbers.Items.Clear();
+                try
+                {
+                    using (StreamReader openList = new StreamReader(ofd.OpenFile()))
+                    {
+                        string line;
+                        int count = 1;
+                        while ((line = openList.ReadLine()) != null)
+                        {
+                            lstList.Items.Add(line);
+                            lstTaskNumbers.Items.Add(count);
+                            count++;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("The file could not be read.", "Problem reading file");
+                }
+
+                checkListForItems(lstList, btnAdd, btnRemove, btnUpdate);
+            }
+        }
+    }
+}
